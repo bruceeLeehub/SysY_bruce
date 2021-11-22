28ceae03541a7cc28039439e7905bdc3ace8e095
周华盛 <19373460@buaa.edu.cn>
2021-11-22 20:45:43 +0800

    33

--- SysY_Compiler-main/src/NoneTerminal/VarDecl.java
+++ SysY_Compiler-main/src/NoneTerminal/VarDecl.java
@@ -1,61 +1,50 @@
 package NoneTerminal;
 
 import MyError.Error;
-import WordAnalyse.IdentifySymbol;
-import WordAnalyse.RegKey;
-import WordAnalyse.Symbol;
+import WordAnalyse.*;
 
 import java.util.ArrayList;
 
 public class VarDecl extends Decl {
-    public static String name = "<VarDecl>";
+    public ArrayList<VarDef> varDef_List = new ArrayList<>();
+    public static String name_varDecl = "<VarDecl>";
+    public BType bType = null;
 
-    private BType bType;
-    private ArrayList<VarDef> varDefList;
+    public static VarDecl analyse(IdentifySymbol identifySymbol) {
+        Symbol sym = identifySymbol.get_CurrentSym();
+        VarDecl varDecl = new VarDecl();
+
+        varDecl.bType = (BType.analyse(identifySymbol));
+        varDecl.varDef_List.add(VarDef.analyse(identifySymbol));
+        while (identifySymbol.get_CurrentSym().getRegKey() == RegKey.COMMA) {
+            identifySymbol.getASymbol();
+            VarDef varDef = VarDef.analyse(identifySymbol);
+            varDecl.varDef_List.add(varDef);
+        }
+        Symbol curSymbol = identifySymbol.get_CurrentSym();
+        RegKey regKey = curSymbol.getRegKey();
+        boolean isSEMICN = regKey == RegKey.SEMICN;
+        if (isSEMICN) {
+            identifySymbol.getASymbol();
+        }
+        else {
+            Symbol preSymbol = identifySymbol.get_PreSym();
+            int rowidx = preSymbol.getRow_Idx();
+            Error.addErrorOutPut(rowidx + " i");
+        } //i: ';' needed
+
+
+        identifySymbol.addStr(name_varDecl);
+        return varDecl;
+    }
 
     public VarDecl() {
-        this.bType = null;
-        this.varDefList = new ArrayList<>();
-    }
-
-    public void setBType(BType bType){
-        this.bType = bType;
-    }
-
-    public void addVarDef(VarDef varDef){
-        this.varDefList.add(varDef);
     }
 
     @Override
     public void genCode(){
-        for(VarDef varDef : varDefList){
+        for(VarDef varDef : this.varDef_List){
             varDef.genCode();
         }
-    }
-
-    public static VarDecl analyse(IdentifySymbol identifySymbol) {
-        Symbol sym;
-        boolean judge = true;
-        VarDecl varDecl = new VarDecl();
-
-
-        sym = identifySymbol.get_CurrentSym();
-        varDecl.setBType(BType.analyse(identifySymbol));
-
-        if (judge) {
-            varDecl.addVarDef(VarDef.analyse(identifySymbol));
-        }
-        while (judge && identifySymbol.get_CurrentSym().getRegKey() == RegKey.COMMA) {
-            identifySymbol.getASymbol();
-            varDecl.addVarDef(VarDef.analyse(identifySymbol));
-        }
-        if (judge && identifySymbol.get_CurrentSym().getRegKey() == RegKey.SEMICN) identifySymbol.getASymbol();
-        else Error.addErrorOutPut(identifySymbol.get_PreSym().getRow_Idx() + " i"); // ERROR -- i: ';' needed
-
-
-        if (judge) {
-            identifySymbol.addStr(name);
-        }
-        return varDecl;
     }
 }
