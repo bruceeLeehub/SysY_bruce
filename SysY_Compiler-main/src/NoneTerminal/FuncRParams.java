062b9995139318c23b02b7b130c7858dd2fba8a4
周华盛 <19373460@buaa.edu.cn>
2021-11-22 08:44:58 +0800

    22

--- SysY_Compiler-main/src/NoneTerminal/FuncRParams.java
+++ SysY_Compiler-main/src/NoneTerminal/FuncRParams.java
@@ -12,31 +12,42 @@ public class FuncRParams {
     public static String name_funcRParams = "<FuncRParams>";
     public static boolean TypeCheck = false;
 
-    public static boolean analyse(IdentifySymbol identifySymbol, My_Int numOfParamsActually, UnaryExp unaryExp) {
-        Symbol sym = identifySymbol.get_CurrentSym();
-        int paramSeq = 1;
+    public static boolean analyse(IdentifySymbol identifySymbol, My_Int actualParamsNum, UnaryExp unaryExp) {
+        Symbol symbol = identifySymbol.get_CurrentSym();
+        RegKey regKey = symbol.getRegKey();
+        boolean rightRegKey = (regKey == RegKey.NOT || regKey == RegKey.INTCON ||
+                regKey == RegKey.IDENFR || regKey == RegKey.LPARENT ||
+                regKey == RegKey.PLUS || regKey == RegKey.MINU);
         Symbol funcSym = identifySymbol.getNearest_PreIdent();
-        TableEntry funcEntry = SymTable.get_SymByNameInAllTable(true, funcSym.get_IdentName());
-        if (sym.getRegKey() == RegKey.IDENFR || sym.getRegKey() == RegKey.LPARENT ||
-                sym.getRegKey() == RegKey.PLUS || sym.getRegKey() == RegKey.MINU ||
-                sym.getRegKey() == RegKey.NOT || sym.getRegKey() == RegKey.INTCON) {
-            numOfParamsActually.my_Int++;
-            TypeCheck = true;
-        }else{
+        String name = funcSym.get_IdentName();
+        TableEntry funcEntry = SymTable.get_SymByNameInAllTable(true, name);
+        if (!rightRegKey) {
             return true;
+        }else{
+            actualParamsNum.my_Int = actualParamsNum.my_Int + 1;
+            TypeCheck = true;
         }
-        unaryExp.addFuncRParam(Exp.analyse(identifySymbol));
-        // ERROR -- e: RParams not match
-        Error.checkRParamsMatched(funcEntry, tbEntryModel.get(tbEntryModel.size() - 1), funcSym, paramSeq++);
-        tbEntryModel.remove(tbEntryModel.size() - 1);
+        int paramSeq = 1;
+        Exp exp = Exp.analyse(identifySymbol);
+        unaryExp.addFuncRParam(exp);
+        // e: rparams do not match
+        int size = tbEntryModel.size();
+        TableEntry actualTE = tbEntryModel.get(size - 1);
+        Error.checkRParamsMatched(funcEntry, actualTE, funcSym, paramSeq);
+        paramSeq = paramSeq + 1;
+        tbEntryModel.remove(size - 1);
         while (identifySymbol.get_CurrentSym().getRegKey() == RegKey.COMMA) {
-            numOfParamsActually.my_Int++;
+            actualParamsNum.my_Int = actualParamsNum.my_Int + 1;
             TypeCheck = true;
             identifySymbol.getASymbol();
-            unaryExp.addFuncRParam(Exp.analyse(identifySymbol));
-            // ERROR -- e: RParams not match
-            Error.checkRParamsMatched(funcEntry, tbEntryModel.get(tbEntryModel.size() - 1), funcSym, paramSeq++);
-            tbEntryModel.remove(tbEntryModel.size() - 1);
+            Exp exp1 = Exp.analyse(identifySymbol);
+            unaryExp.addFuncRParam(exp1);
+            // e: rparams do not match
+            size = tbEntryModel.size();
+            actualTE = tbEntryModel.get(size - 1);
+            Error.checkRParamsMatched(funcEntry, actualTE, funcSym, paramSeq);
+            paramSeq = paramSeq + 1;
+            tbEntryModel.remove(size - 1);
         }
 
         identifySymbol.addStr(name_funcRParams);
--- SysY_Compiler-main/src/NoneTerminal/FuncType.java
+++ SysY_Compiler-main/src/NoneTerminal/FuncType.java
@@ -1,37 +1,32 @@
 package NoneTerminal;
 
-import ParcelType.My_DataType;
-import Table.DataType;
-import WordAnalyse.IdentifySymbol;
-import WordAnalyse.RegKey;
-import WordAnalyse.Symbol;
+import ParcelType.*;
+import Table.*;
+import WordAnalyse.*;
 
 public class FuncType {
-    public static String name = "<FuncType>";
-
-    private RegKey regKey;
-    public FuncType(RegKey regKey){
-        this.regKey = regKey;
-    }
+    private final RegKey regKey;
+    public static String name_funcType = "<FuncType>";
 
     public RegKey getRegKey(){
         return this.regKey;
     }
 
     public static FuncType analyse(IdentifySymbol identifySymbol, My_DataType dataType) {
-        Symbol sym = identifySymbol.get_CurrentSym();
-        boolean judge = true;
-        FuncType funcType = new FuncType(sym.getRegKey());
-        judge &= (sym.getRegKey() == RegKey.VOIDTK || sym.getRegKey() == RegKey.INTTK);
+        Symbol symbol = identifySymbol.get_CurrentSym();
+        RegKey regKey = symbol.getRegKey();
+        boolean judge = (regKey == RegKey.VOIDTK || regKey == RegKey.INTTK);
         if (judge) {
-            dataType.dataType = DataType.VOID_DATA;
-            if (sym.getRegKey() == RegKey.INTTK) dataType.dataType = DataType.INT_DATA;
             identifySymbol.getASymbol();
+            dataType.dataType = (regKey == RegKey.INTTK) ? DataType.INT_DATA : DataType.VOID_DATA;
+
+            identifySymbol.addStr(name_funcType);
         }
 
-        if (judge) {
-            identifySymbol.addStr(name);
-        }
-        return funcType;
+        return new FuncType(regKey);
+    }
+
+    public FuncType(RegKey regKey){
+        this.regKey = regKey;
     }
 }
